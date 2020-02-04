4b872fbd21f39ee336047acb2a3217e9fc2e3f53
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-04 22:42:47 +0100

    feat: Proved that a final singleton class is not tastable because I can't create subclassed so I can't create a mock

--- Singleton/Singleton.swift
+++ Singleton/Singleton.swift
@@ -20,7 +20,7 @@ struct LoggedInUser {}
 //   For example, if you have an object that performs some really intense algorithm to determine the number of faces in a picture, make the numberOfFaces
 //   property lazily initialized.
 
-class APIClient {
+final class APIClient {
 
     // this is constant and lazy loaded
     
@@ -29,6 +29,8 @@ class APIClient {
     // Prevent people to create an APIClient
 
     private init() {}
+    
+    func login(completion: (LoggedInUser)->Void) {}
 }
 
 /// singleton (convenience method in Swift system libraries)
@@ -36,3 +38,20 @@ class APIClient {
 //URLSession()
 
 let client = APIClient.instance
+
+// How do I test a Singleton ??
+
+class LoginVC: UIViewController {
+    
+    func didTapLoginButton() {
+        
+        // How can you override the login function inside the APIClient instance in order to provide a mock one?
+        // If the class is final, I can't subclass it
+        // instance is a let, so I can't changed it
+        // APIClient -> impossible to test !!
+        
+        APIClient.instance.login { user in
+            
+        }
+    }
+}
