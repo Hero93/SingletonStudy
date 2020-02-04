e2e5cb9fabb17b1b59e34c33d516bdfe325f2965
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-04 21:48:01 +0100

    first commit

---
+++ Singleton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Singleton
+//
+//  Created by Luca LG. Gramaglia on 03/02/2020.
+//  Copyright © 2020 e-Gate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
