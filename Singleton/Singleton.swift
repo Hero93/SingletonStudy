ab532014484023e011db16146283afe69a679446
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-04 21:48:46 +0100

    feat: singleton by the book implementation, not using swift tools

---
+++ Singleton/Singleton.swift
@@ -0,0 +1,28 @@
+//
+//  Singleton.swift
+//  Singleton
+//
+//  Created by Luca LG. Gramaglia on 03/02/2020.
+//  Copyright © 2020 e-Gate. All rights reserved.
+//
+
+import UIKit
+
+/// Singleton (by the book implementation, not using Swift tools)
+
+class APIClient {
+
+    private static let instance = APIClient()
+
+    static func getInstance() -> APIClient {
+        return instance
+    }
+
+    // Prevent people to create an APIClient
+
+    private init() {}
+}
+
+let client = APIClient.getInstance()
+
+struct LoggedInUser {}
