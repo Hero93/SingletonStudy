c6d2b126f92d9432b0fdb0f5963530410a9ab612
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-05 22:33:34 +0100

    feat: solution to make code more flexible is making the features (Login & Feed module) depends on the concrete type (APIClient) by introducing the concept of "Interfaces" (closures or protocols). In this way the module doesn't need to know where the data is coming from

--- Singleton/Singleton.swift
+++ Singleton/Singleton.swift
@@ -8,59 +8,53 @@
 
 import UIKit
 
-/// Singleton in Swift
-
-// API Module: We have generic client
-
-class APIClient {
-    
-    static let shared = APIClient()
-
-    func execute(_ : URLRequest, completion: (Data)->Void) {}
-}
-
-// Login Module: Specialized client for the Login module
-
 struct LoggedInUser {}
 
-extension APIClient {
-    func login(completion: (LoggedInUser)->Void) {}
-}
+// Main Module
 
-// Feed Module: Specialized client for the Feed module
+protocol LoginProtocol {
+    func login(completion: (LoggedInUser)->Void)
+}
 
 struct FeedItem {}
 
-extension APIClient {
-    func loadFeed(completion: ([FeedItem])->Void) {}
-}
-
 // How do I test a Singleton ??
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, LoginProtocol {
     
-    var api = APIClient.shared
+    // "Protocol" example
+    func login(completion: (LoggedInUser) -> Void) {}
     
     func didTapLoginButton() {
-                
-        // By creating the extensions I didn't break the login call.
-        
+                        
         // The client it's still shared between the module.
         // If I want to add a new Module that UPLOADS data, I need to make a new method in the APIClient and so you break the other clients.
         // How to make it more flexible?
         
-        api.login { user in }
+        // SOULTION (STAGE 2): Dependency Inversion -> What if instead of the module depending of the concrete APIClient,
+        //                                             we make a concreate APIClient depend on the modules.
+        
+        // APIClient it's just a detail, it's not important to the business. So if preferable to have contained logic in modules and details depend on the features,
+        // in this case the features depending on the APIClient.
+        
+        // To do so, we introduce "Protocol" or "Closures" or any type of Interface for the module.
+        // The "Feed" module just need a function to load the feed data, it doesn't need to know where the data is coming from.
+        
+        // When we do "APIClient.shared.api.login", we depend on APIClient, we need to know that we have to call the "login" function.
+        
+        // Working with Protocols and dependency injection it's a great way to improve.
+        // This code it's also very testable because we don't need a web service to load the data, we can mock data.
     }
 }
 
 class FeedVC: UIViewController {
     
-    var api = APIClient.shared
-    
+    // "Closure" example
+    var loadFeed: (([FeedItem])->Void)?
+
     override func viewDidLoad() {
         
         // By creating the extensions I didn't break the loadFeed call
             
-        api.loadFeed { feed in }
     }
 }
